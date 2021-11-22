eaca67903ce548e5de999b93402d542ad75fdf3d
sukmajn <91167840+sukmajn@users.noreply.github.com>
2021-11-23 05:41:44 +0700

    Add files via upload

---
+++ elkom1prak8_sukmajn.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 23 05:38:32 2021
+
+@author: Sukma Julia Nada
+"""
+
+print("PROGRAM MEMUNCULKAN KARAKTER INDEKS GANJIL")
+masuk = input("Masukkan sebuah kata: ")
+
+print("Karakter indeks ganjil:", masuk[1::2]
